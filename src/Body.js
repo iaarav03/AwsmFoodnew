febdb55c6a3f858b88f645e3660582cf1742cab0
iaarav03 <aarav.21ug2059@iiitranchi.ac.in>
2024-04-07 23:53:26 +0530

    scndd

--- src/Body.js
+++ src/Body.js
@@ -17,7 +17,7 @@ const Body = () => {
   async function getRestaurants() {
     try {
       const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await response.json();
 
